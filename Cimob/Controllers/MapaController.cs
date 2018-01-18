c44d0ba6545481d7d873bd2f5188e8afc83604a2
MarleneCunha14 <marlenecunha14@hotmail.com>
2018-01-18 17:48:35 +0000

    Sprint3

--- Cimob/Controllers/MapaController.cs
+++ Cimob/Controllers/MapaController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Cimob.Data;
+using Cimob.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -24,5 +25,22 @@ namespace Cimob.Controllers
             ViewData["Id"] = new SelectList(_context.Mapa, "Id", "Adress");
             return View();
         }
+
+        public JsonResult GetAllLocation()
+        {
+            var data = _context.Mapa.ToList();
+
+            return Json(data);
+        }
+
+
+        public JsonResult GetAllLocationById( int id)
+        {
+            var data = _context.Mapa.Where(x => x.Id == id).SingleOrDefault();
+
+            return Json(data);
+        }
+
+        
     }
 }
